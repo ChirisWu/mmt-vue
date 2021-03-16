20ebbec1c67c6d139a8035b5a2e437340d9a01dc
chiriswu <chiriswu@tencent.com>
2021-03-16 20:33:38 +0800

    首页静态

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,141 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="home_header">
+        <img src="../assets/home/header.png">
+    </div>
+      <div class="home_list_wrapper">
+        <el-row>
+          <el-col :span="8">
+            <div class="item_card">
+                <img src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg">
+                <div class="img-tips">
+                  <p>我们都在寻找一个地方，寻找一种声音。或许漂泊，四海为家，江边饮酒，月前奏琴</p>
+                </div>
+                <div class="item_text"><p>音乐广场</p></div>
+            </div>
+          </el-col>
+          <el-col :span="8">
+            <div class="item_card">
+              <img src="https://fuss10.elemecdn.com/0/6f/e35ff375812e6b0020b6b4e8f9583jpeg.jpeg">
+              <div class="img-tips">
+                <p>我们都在寻找一个地方，寻找一种声音。或许漂泊，四海为家，江边饮酒，月前奏琴</p>
+              </div>
+              <div class="item_text"><p>音乐动态</p></div>
+            </div>
+          </el-col>
+          <el-col :span="8">
+            <div class="item_card">
+              <img src="https://cube.elemecdn.com/6/94/4d3ea53c084bad6931a56d5158a48jpeg.jpeg">
+              <div class="img-tips">
+                <p>我们都在寻找一个地方，寻找一种声音。或许漂泊，四海为家，江边饮酒，月前奏琴</p>
+              </div>
+              <div class="item_text"><p>我的主页</p></div>
+            </div>
+          </el-col>
+        </el-row>
+      </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+
+  },
+  data() {
+    return {
+    }
+  },
+  methods: {
+
+  },
+  mounted() {
   }
+
 }
 </script>
+
+<style lang="less" scoped>
+  .home{
+    width: 100%;
+    height: 100%;
+  }
+  .home_header{
+    background-color: azure;
+    width: 70%;
+    margin: 0 auto;
+    height: 270px;
+    img{
+      height: 100%;
+      width: 100%;
+    }
+  }
+  .home_list_wrapper{
+    width: 70%;
+    height: 600px;
+    margin: 0 auto;
+    .el-row {
+      margin-bottom: 20px;
+      height: 500px;
+      &:last-child {
+        margin-bottom: 0;
+      }
+    }
+    .el-col {
+      border-radius: 4px;
+      padding: 0 10px;
+      box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1)
+    }
+    .item_card{
+      height: 80%;
+      width: 80%;
+      margin: 30px auto;
+      position: relative;
+      background-color: aliceblue;
+
+      img{
+        width: 100%;
+        height: 100%;
+        margin-top: 40px;
+        display: block;
+        transition: all 0.4s;
+      }
+      .img-tips{
+        text-align: center;
+        font-size: 19px;
+        color: rgb(255,255,255);
+        background-color: #823131;
+        opacity: 0.4;
+        width: 100%;
+        height: 80%;
+        border: 1px solid white;
+        position: absolute;
+        top: 0;
+        p{
+          margin-top: 50%;
+        }
+        display: none;
+      }
+      .item_text{
+        text-align: center;
+        font-size: 23px;
+        color: white;
+        position: absolute;
+        width: 100%;
+        height: 20%;
+        bottom: 0;
+      }
+    }
+    .item_card:hover img{
+      transform: scale(1.2);
+    }
+    .item_card:hover .img-tips{
+        display: block;
+    }
+  }
+
+</style>
